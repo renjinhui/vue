3caa4520d40a95c0d76d5173e2d7a0f7d0d6fb99
renjinhui <1227760869@qq.com>
2018-03-09 07:23:04 +0800

    央视修改

--- vuetest/src/views/index.vue
+++ vuetest/src/views/index.vue
@@ -2,23 +2,65 @@
     .main{
         width: 100%;
         height: 100%;
-        background: rgba(0,0,0,0.6);
+        position: relative;
+        .title_box{
+            width: 100%;
+            height: 50px;
+            line-height: 50px;
+            background:#eae1e1;
+        }
         .menu_box{
             display: inline-block;
+            position: relative;
+            width: 200px;
+            height: 100%;
+            background: #948a9c;
+            color: #fff;
+            li>a{
+                display: inline-block;
+                width: 100%;
+                height: 100%;
+                text-align: center;
+                color: #fff;
+                height:30px;
+                line-height:30px; 
+            }
+        }
+        .body_box{
+            position:absolute;
+            top:50px;
+            bottom:0;
+            left:0;
+            right:0;
+        }
+        .content_box{
+            // position: absolute;
+            // left:205px;
+            // right:0;
+            // top:0;
+            // bottom:0;
+            margin-left:205px;
         }
     }
 </style>
 <template>
     <div class="main">
-        <ul class="menu_box">
-            <li>
-                <router-link to='/activity'>活动</router-link>
-            </li>
-        </ul>
-        <div class="content_box">
-            这是内容部分  这是在nrc分支
-            分支管理---------正在测试
+        <div class="title_box">
+            <div class="lt">icon</div>
+            <div class="name rt">任金辉</div>
         </div>
+        <div class="body_box">
+            <ul class="menu_box lt">
+                <li>
+                    <router-link to='/activity'>活动</router-link>
+                </li>
+            </ul>
+            <div class="content_box">
+                这是内容部分  这是在nrc分支
+                分支管理---------正在测试
+            </div>    
+        </div>
+        
     </div>
 </template>
 
